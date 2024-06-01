e00766851aacac61792f193aa182825cec4d4085
Pedro Rodrigues <pedrogarodrigues@gmail.com>
2024-06-01 17:55:13 +0100

    Update dependencies and replace prettier config file

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/**
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false
+};
